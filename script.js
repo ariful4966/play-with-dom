d7de89ea88dc80187d5f898b56ad737f7efc255e
ariful4082 <ariful4082@gmail.com>
2021-03-26 21:22:37 +0600

    select sibling of element

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const  chiildren = document.querySelector('.item')
+const  chiildrenOne = document.querySelector('.item')
 // const parent = chiildren.parentElement;
-const grandparent = chiildren.closest('.todo-list')
-console.log(grandparent);
+// const grandparent = chiildren.closest('.todo-list')
+const childrenTwo = chiildrenOne.nextElementSibling
+childrenTwo.style.color = 'orange'
+console.log(childrenTwo);
