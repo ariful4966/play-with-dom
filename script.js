015e82f373ecd73bb094093483c8cbb1b58e4d10
ariful4082 <ariful4082@gmail.com>
2021-03-26 21:19:37 +0600

    select grandparent using children with closest method

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const  chiildren = document.querySelector('.item')
-const parent = chiildren.parentElement;
-console.log(parent);
+// const parent = chiildren.parentElement;
+const grandparent = chiildren.closest('.todo-list')
+console.log(grandparent);
