225d19c60d502999494ab1eacd435559394fbbf6
ariful4082 <ariful4082@gmail.com>
2021-03-26 22:28:05 +0600

    insert div in html file

--- script.js
+++ script.js
@@ -2,5 +2,11 @@
  const divElement = document.createElement('div');
  
  divElement.className = 'red'
+ divElement.setAttribute('id', 'red')
+ divElement.setAttribute('title', 'Ariful Islam')
+ divElement.innerText='Hello World'
+ const container = document.querySelector('.todo-list')
+ const h2Element = container.querySelector('h2');
+ container.insertBefore(divElement, h2Element)
 
  console.log(divElement);
