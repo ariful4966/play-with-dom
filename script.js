9a3345d6d6f31c2bff1dcdf12c8eb68e0d282045
ariful4082 <ariful4082@gmail.com>
2021-03-26 21:16:44 +0600

    get parentelement by the children selector

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const grandParent = document.querySelector('.todo-list')
-// const parent = grandParent.children;
-const children = grandParent.querySelectorAll('.item')
-
-console.log(children);
+const  chiildren = document.querySelector('.item')
+const parent = chiildren.parentElement;
+console.log(parent);
